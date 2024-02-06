6e88553857666fda41afe22b4d5803934edf7f59
Justine Garandel <jg.garandel@hotmail.fr>
2024-02-06 15:15:54 +0100

    update file role class/controller

--- Back/App/controllers/RoleController.php
+++ Back/App/controllers/RoleController.php
@@ -1,3 +1,60 @@
 <?php
 
-require_once('RoleModel.php');
+require_once('../models/Role.php');
+
+class RoleController 
+{
+  public function createRole($name_role) {
+
+    try {
+      $role = new Role(null, $name_role);
+      $role->insertWithValidation();
+
+      echo "Le role a été crée.";
+
+    }catch (PDOException $erreur) {
+
+      echo "Erreur lors de la création du role : " . $erreur->getMessage();
+    }
+  }
+
+  public function readRole() {
+
+    $roles = Role::getAllRoles();
+    echo "Liste des rôles : <br>";
+    foreach ($roles as $role) {
+      echo $role['name_role'] . "<br>";
+    }
+  }
+
+  public function updateRole($id_role, $name_role) {
+
+    try {
+
+      $role = new Role($id_role);
+      $role->name_role = $name_role;
+      $role->update();
+
+      echo "Le role a été mis à jours avec succes.";
+
+    } catch (PDOException $erreur) {
+
+      echo "Erreur lors de la mise a jour du role : " . $erreur->getMessage();
+    }
+  }
+
+  public function deleteRole($id_role) {
+
+    try{
+
+      $role = new Role($id_role);
+      $role->delete();
+
+      echo "Le rolea été supprimé.";
+
+    }catch (PDOException $erreur) {
+      
+      echo "Erreur lors de la suppression du rôle : " . $erreur->getMessage();
+    }
+  }
+}
--- Back/App/models/Role.php
+++ Back/App/models/Role.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once('D:\ENV\Logiciel\XAMPP\htdocs\QuaiQuantique\Back\config\Database.php');
+require_once('./Back/config/Database.php');
 
-class RoleModel {
-
+class Role 
+{
   private $id_role;
   private $name_role;
   private $connexion;
@@ -12,22 +12,20 @@ class RoleModel {
   {
     $this->id_role = $id_role;
     $this->name_role = $name_role;
+    $this->connexion = $this->getDBConnection();
   }
-
   //connexion BDD
   private function getDBConnection() {
-    return Database::getConnection();
+    $db = Database::getInstance();
+    return $db->getConnexion();
   }
-
 
   //ajouter un role
   public function insert() {
 
-    $db = $this->getDBConnection();
-
     try {
 
-      $requetes = $db->prepare('INSERT INTO `role`(`name_role`) VALUES (:$name_role)');
+      $requetes = $this->connexion->prepare('INSERT INTO `role`(`name_role`) VALUES (:$name_role)');
       $requetes->bindParam(':name_role', $this->name_role);
       $requetes->execute();
 
@@ -36,16 +34,12 @@ class RoleModel {
       echo "Erreur d'insertion : " . $erreur->getMessage();
     }
   }
-
-
   //lecture des role en fonction des user
   public function select() {
 
-    $db = $this->getDBConnection();
-
     try {
 
-      $requetes = $db->prepare('SELECT `name_role` FROM `role`');
+      $requetes = $this->connexion->prepare('SELECT `name_role` FROM `role`');
       $requetes->execute();
 
     } catch (PDOException $erreur) {
@@ -53,16 +47,12 @@ class RoleModel {
       echo "Erreur de selection : " . $erreur->getMessage();
     }
   }
-
-
   //changer les role 
   public function update() {
 
-    $db = $this->getDBConnection();
-
     try {
       
-      $requetes = $db->prepare('UPDATE `role` SET `name_role` = :name_role WHERE `id_role` = :id_role');
+      $requetes = $this->connexion->prepare('UPDATE `role` SET `name_role` = :name_role WHERE `id_role` = :id_role');
       $requetes->bindParam(':name_role', $this->name_role);
       $requetes->bindParam(':id_role', $this->id_role);
       $requetes->execute();
@@ -72,16 +62,12 @@ class RoleModel {
       echo "Erreur d'update : " . $erreur->getMessage();
     }
   }
-
-
   //supp un role
   public function delete() {
 
-    $db = $this->getDBConnection();
-
     try {
 
-      $requetes = $db->prepare('DELETE FROM `role` WHERE `id_role` = :id_role');
+      $requetes = $this->connexion->prepare('DELETE FROM `role` WHERE `id_role` = :id_role');
       $requetes->bindParam(':id_role', $this->id_role);
       $requetes->execute();    
 
@@ -91,4 +77,17 @@ class RoleModel {
     }
 
   }
+
+  //verification nbr caractères
+  public function validateName() {
+    
+    if (strlen($this->name_role) < 3) {
+      throw new Exception("Le nom du role doit cmporter au moins 3 caractères.");
+    }
+  }
+  public function insertWithValidation() {
+    $this->validateName();
+    $this->insert();
+  }
+
 }
