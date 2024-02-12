ae9a515586ebd934a757d5b24e921f7fedeac0dc
Ruyssen Romain <ruyssensud@gmail.com>
2024-02-12 15:11:46 +0100

    first commit create autoloader.php and buid rooter.php

---
+++ Back/config/rooter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Back\config;
+
+class Rooter
+{
+    public function start()
+    {
+        echo 'bonjour';// creer l'uri rooter et instancier les controller et les action 
+    }
+}
